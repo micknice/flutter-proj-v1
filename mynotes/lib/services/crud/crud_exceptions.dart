c6b692fc4f93008b8ff980df7e5743e548ece241
micknice <mikemccullagh@outlook.com>
2023-05-16 15:26:00 +0100

    adds new notes, routes and appbar button

---
+++ mynotes/lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class UserDoesNotExist implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class NoteDoesNotExist implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
