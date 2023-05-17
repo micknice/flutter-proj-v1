c85bbb1e66f78883bb1c0e2f99d317b270cfec3d
micknice <mikemccullagh@outlook.com>
2023-05-17 01:47:56 +0100

    adds displays stored notes in notes view, fixes to calcs

--- mynotes/lib/utils/calcs.dart
+++ mynotes/lib/utils/calcs.dart
@@ -1,7 +1,7 @@
 String calculateNaturalFlow(
     roomArea, roomHeight, openingArea, typicalWindSpeed) {
   final rmAr = double.parse(roomArea);
-  final rmHght = double.parse(roomArea);
+  final rmHght = double.parse(roomHeight);
   final opngAr = double.parse(openingArea);
   final wndSpd = double.parse(typicalWindSpeed);
   double volume = rmAr * rmHght;
